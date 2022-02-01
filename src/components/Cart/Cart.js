60eb3c21cbff516f3b4a8396c0fadd4b961abb5b
GabrielMoranduzzo <gemoranduzzo@hotmail.com>
2022-02-01 01:15:04 -0300

    Item Collection 2

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,53 @@
 import { useCartContext } from "../../context/cartContext"
 import { Link } from 'react-router-dom'
 import './Cart.css'
+import { addDoc, collection, documentId, getDocs, getFirestore, query, where, writeBatch } from "firebase/firestore"
+
+
 
 const Cart = () => {
 
     const { cartList, eliminarItem, vaciarCarrito, total } = useCartContext()
 
+    const realizarCompra = async () => {
+        let orden = {}
+
+        orden.buyer = {nombre: 'Gabriel', email: 'g@gmail.com', tel: '123456789'}
+        orden.total = total();
+
+        orden.items = cartList.map(cartItem => {
+            const id = cartItem.id;
+            const nombre = cartItem.title;
+            const precio = cartItem.price * cartItem.cantidad;
+            const cantidad = cartItem.cantidad
+
+            return(id, nombre, precio, cantidad)
+        })
+
+        const db = getFirestore()
+        const ordenCollection = collection(db, 'orders')
+        await addDoc(ordenCollection, orden)
+        .then(resp => console.log(resp))
+        .catch(err => console.log(err))
+        .finally(()=> console.log('cargando'))
+
+        const queryCollection = collection(db, 'items')
+
+        const queryActualizarStock = query(
+            queryCollection, where( documentId(), 'in', cartList.map(it => it.id)))
+            const batch = writeBatch(db)
+
+            await getDocs(queryActualizarStock)
+            .then(resp => resp.docs.forEach(res => batch.update(res.ref, {
+                stock: res.data().stock - cartList.find(item => item.id === res.id).cantidad
+                })
+            ))
+            .catch(err => console.log(err))
+            .finally(()=> console.log('stock actualizado'))
+
+            batch.commit()
+    }
+ 
     return (
         <>
             {cartList.length === 0 ? (
@@ -29,6 +71,7 @@ const Cart = () => {
                         <div className="cartFooter">
                         <h3>Total = ${total()}</h3>
                         <button className='btn btn-danger' onClick={vaciarCarrito}>Vaciar Carrito</button>
+                        <button className='btn btn-danger' onClick={realizarCompra}>Generar Orden</button>
                         </div>
                 </>
             )}
